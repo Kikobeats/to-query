7dee2c3d4636d9cff89bbd29356434c3f4401567
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-05-23 01:09:34 +0200

    Update parse.js

--- src/parse.js
+++ src/parse.js
@@ -1,8 +1,7 @@
 'use strict'
 
-const { flow } = require('lodash')
-
 const { parse: parseQuery } = require('querystring')
 const { parse: parseUrl } = require('url')
+const { flow } = require('lodash')
 
 module.exports = flow([url => parseUrl(url).query, parseQuery])

3f83b5271bc576d89da4a8824e61bc624776b9ed
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-08-22 12:11:05 +0200

    test: split tests

--- test/parse.js
+++ test/parse.js
@@ -3,13 +3,19 @@
 const test = require('ava')
 const parse = require('../parse')
 
-test('to object', t => {
+test('plain object', t => {
   t.deepEqual(parse('/?foo=bar'), {
     foo: 'bar'
   })
 })
 
-test('nested', t => {
+test('decode url', t => {
+  t.deepEqual(parse('/?url=https%3A%2F%2Fedna-pxmb461cy.now.sh'), {
+    url: 'https://edna-pxmb461cy.now.sh'
+  })
+})
+
+test('nested objects', t => {
   t.deepEqual(
     parse(
       '/?url=https%3A%2F%2Fkikobeats.com&data.avatar.selector=%23avatar&data.avatar.attr=src&data.avatar.type=image&force=true&data.resume.selector=h1&data.resume.attr=text'
